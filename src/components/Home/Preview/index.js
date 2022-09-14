4466098eab37d6f6f2c53859f3d778ac827bf211
swilliams404 <Shaquille404@gmail.com>
2022-09-13 21:55:39 -0400

    Updating Preview Content Piece

--- src/components/Home/Preview/index.js
+++ src/components/Home/Preview/index.js
@@ -1,24 +1,47 @@
 import './index.scss'
 import Carousel from './Reusable/image-gallery.component'
+import Water from './../../../assets/images/Circle_Watering_Can.png'
+import Table from './../../../assets/images/Circle_Table.png'
+import Card from './../../../assets/images/Card_3.png'
+import Pie from './../../../assets/images/Circle_Pie.png'
+import Wood from './../../../assets/images/Circle_Wood.png'
 
 const Preview = () => {
+  const slides = [
+    { url: './Reusable/image-1.png' },
+    { url: './Reusable/image-2.png' },
+    { url: './Reusable/image-3.png' },
+    { url: './Reusable/image-4.png' },
+  ]
   return (
     <div className="preview-container">
       <div className="preview-wrapper">
         <div className="preview-card-container">
           <div className="preview-left-icons">
-            {/* <img src={Water} alt="water" />
-            <img src={Table} alt="table" /> */}
+            <img src={Water} alt="water" />
+            <img src={Table} alt="table" />
           </div>
           <div className="preview-card">
-            {/* <img src={Card} alt="Preview-card" /> */}
+            <img src={Card} alt="Preview-card" />
+            <h1>And More!</h1>
+            <div className="preview-text-body">
+              <p>
+                Farm, Build, Explore, Relax, Decorate, Quest- spend your time
+                doing whatever makes you happy!
+                <p>
+                  Enjoy the story or play the game in Free Mode to head straight
+                  into play without story related blockades.
+                </p>
+              </p>
+            </div>
           </div>
-          <div className="preview-right-icons"></div>
-          {/* <img src={Pie} alt="Pie" />
-          <img src={Wood} alt="wood" /> */}
+          <div className="preview-right-icons">
+            <img src={Pie} alt="Pie" />
+            <img src={Wood} alt="wood" />
+          </div>
         </div>
         <div className="preview-hero"></div>
-        <Carousel />
+        <Carousel slides={slides} />
       </div>
     </div>
   )
