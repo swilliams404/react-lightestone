4d76b8b19b82d12196780a00dae523d91ea08093
swilliams404 <Shaquille404@gmail.com>
2022-09-13 21:54:45 -0400

    Changes to Carousel WIP

--- src/components/Home/Preview/Reusable/image-gallery.component.js
+++ src/components/Home/Preview/Reusable/image-gallery.component.js
@@ -1,48 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.scss'
 
-const Carousel = () => {
+const Carousel = ({ slides }) => {
+  const [currentIndex, setCurrentUser] = useState(0)
+
+  const slideStyles = {
+    width: '100%',
+    height: '100%',
+    borderRadius: '1rem',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundImage: `url(${slides[currentIndex].url})`,
+  }
+
   return (
-    <div className="slider">
-      <div className="buttons">
-        <button id="prev">
-          <i className="fas fa-arrow-left"></i>
-        </button>
-        <button id="prev">
-          <i className="fas fa-arrow-right"></i>
-        </button>
-      </div>
-      <div className="slide current">
-        <div className="content">
-          <h1>Slide One</h1>
-        </div>
-      </div>
-
-      <div className="slide">
-        <div className="content">
-          <h1>Slide Two</h1>
-        </div>
-      </div>
-
-      <div className="slide">
-        <div className="content">
-          <h1>Slide Three</h1>
-        </div>
-      </div>
-
-      <div className="slide">
-        <div className="content">
-          <h1>Slide Four</h1>
-        </div>
-      </div>
-
-      <div className="slide">
-        <div className="content">
-          <h1>Slide Five</h1>
-        </div>
-      </div>
-
-      <script src="index.js"></script>
+    <div className="carousel-container">
+      <div className="slide-container" style={slideStyles}></div>
     </div>
   )
 }
